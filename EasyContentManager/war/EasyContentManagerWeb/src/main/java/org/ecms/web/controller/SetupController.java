51ce721ee14e9122398931336dcfee10ec11c620
ecms <sujitmajudar2@gmail.com>
2016-01-14 23:34:26 +0530

    Initial

---
+++ EasyContentManager/war/EasyContentManagerWeb/src/main/java/org/ecms/web/controller/SetupController.java
@@ -0,0 +1,18 @@
+package org.ecms.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SetupController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String setUp(ModelMap model) {
+		System.out.println("Hello");
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		return "hello";
+	}
+
+}
